75800923a52c203dfed753db3a7384f6f6c69870
Hector <hectorberryg@gmail.com>
2019-10-11 10:51:23 +0200

    Track component added, bootstrap stylings added

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
+import Spinner from '../layout/Spinner';
+import Track from './Track';
 
 class Tracks extends Component {
     render() {
         return(
             <Consumer>
                 {value => {
-                    console.log(value);
-                    return <h1>Tracks</h1>
+                    const { tracklist, heading } = value;
+                    if(tracklist ===undefined || tracklist.length === 0) {
+                        return <Spinner />
+                    }else {
+                        return (
+                            <React.Fragment>
+                                <h3 className = 'text-center mb-4'>{heading}</h3>
+                                <div className = 'row'>
+                                    {tracklist.map( item => (
+                                        <Track key={item.track.track_id}  track={item.track}/>
+                                    ))}
+                                </div>
+                            </React.Fragment>
+                        )
+                    }
+                    
                 }}
             </Consumer>
         );
